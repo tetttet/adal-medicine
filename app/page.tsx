d44661fde57be186169cdf5e8793b875d26d72bb
Turan-Yahya Gazizuly <turan-yahyagazizuly@Turan-Yahyas-MacBook-Pro.local>
2025-07-09 21:45:16 +0500

    remove call

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { ImagesSliderDemo } from "@/components/Demo/ImagesSliderDemo";
-import CallToAction from "@/components/Element/CallToAction";
+// import CallToAction from "@/components/Element/CallToAction";
 import Causes from "@/components/Home/Causes";
 import FutureEvents from "@/components/Home/FutureEvents";
 import Help from "@/components/Home/Help";
 import Hero from "@/components/Home/Hero";
 import Newsletter from "@/components/Home/NewsLetter";
-import Testimonial from "@/components/Home/Testimonial";
+import Tesstimonial from "@/components/Home/Testimonial";
 import UrgentDonation from "@/components/Home/UrgentDonation";
 import Volunteer from "@/components/SharedComponent/Volunteer";
 import Banner from "@/components/UI/Banner";
@@ -14,7 +14,6 @@ import Pricing from "@/components/UI/Pricing";
 export default function Home() {
   return (
     <>
-      <CallToAction />
       <Banner />
       <ImagesSliderDemo />
       <Help />
